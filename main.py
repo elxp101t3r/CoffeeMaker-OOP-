bbb7fa4d1bb0beffda35c23d467899acb2daa1cf
elxp101t3r <eliotx10101@outlook.com>
2023-11-05 18:48:42 +0200

    Completed!

---
+++ main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+option = menu.get_items()
+
+power_on = True
+
+while power_on:
+    choice = input(f'what coffee do you want: ({option})')
+    if choice == 'off':
+        power_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
